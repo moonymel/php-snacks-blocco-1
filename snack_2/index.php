0be13a9254b682692cd2629e80c540330d8a04b7
moonymel <musumeci.me@gmail.com>
2024-01-24 14:48:12 +0100

    [UPDATE] completed html structure in snack_2

---
+++ snack_2/index.php
@@ -0,0 +1,36 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <form action="dashboard.php" method="GET">
+                <div class="col-6 my-4">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Nome</label>
+                        <input type="text" class="form-control" id="name" name="name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="text" class="form-control" id="email" name="email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="age" class="form-label">Età</label>
+                        <input type="text" class="form-control" id="age" name="age">
+                    </div>
+                    <button type="submit" class="btn btn-success">Invia</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
